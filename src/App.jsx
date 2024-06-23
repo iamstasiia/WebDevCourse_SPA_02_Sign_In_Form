579c255cf24e8b05651d5eb512e9a338e61dbe0b
Anastasiia <anastasiia.khrypko@dci-student.org>
2024-06-23 23:12:33 +0200

    footer added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import "./App.scss";
 import "./components/login-form/Login.components.scss";
 import GreetComponent from "./components/login-form/Greet.comp";
 import { LoginFormContextProvider } from "./contexts/LoginForm.context";
+import Footer from "./components/Footer.component";
 
 function App() {
     return (
-        <LoginFormContextProvider>
-            <div className="form-container">
-                <GreetComponent />
+        <>
+            <LoginFormContextProvider>
+                <div className="form-container">
+                    <GreetComponent />
 
-                <div className="entry">
-                    <SignInComponent />
-                    <SignUpComponent />
+                    <div className="entry">
+                        <SignInComponent />
+                        <SignUpComponent />
+                    </div>
                 </div>
-            </div>
-        </LoginFormContextProvider>
+                
+            </LoginFormContextProvider>
+
+            <Footer />
+        </>
     );
 }
 
---
+++ src/components/Footer.component.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  
+    return (
+      <footer>
+        <small>
+          &copy;2024 Anastasiia Khrypko. All rights reserved. This website is created for personal practice and educational purposes only.
+        </small>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
